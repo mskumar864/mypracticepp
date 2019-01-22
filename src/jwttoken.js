47b633c284697d1086cc9de08354aa41cbef110b
Medasani <smedasani@paypal.com>
2019-01-22 15:03:55 +0530

    india liveaccount

--- src/jwttoken.js
+++ src/jwttoken.js
@@ -1,6 +1,6 @@
 var jwt = require('jsonwebtoken');
   var token = jwt.sign({"iss": "ATwXFTUsC0G1aij_4EK6WEzr_8cd8kfFRHkJxwtNyLQ4yZsEk2p9HXzNNcZ1Xyrc1irtscu2XPIwaSaq",
-  "email":"inweb1@test.com"},null, { algorithm: 'none' });
+  "email":"inisu20@test.com"},null, { algorithm: 'none' });
   console.log(token);
 
   jwt.verify(token, null, { algorithm: 'none' },function(err, decoded) {
